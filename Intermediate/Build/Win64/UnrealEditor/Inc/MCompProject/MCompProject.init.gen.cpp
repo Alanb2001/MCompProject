c34b37fc893a2a75b9ed912cbb8efba45975e929
Alan Bednarski <78023436+Alanb2001@users.noreply.github.com>
2023-01-25 13:33:19 +0000

    Initial Unreal project commit

---
+++ Intermediate/Build/Win64/UnrealEditor/Inc/MCompProject/MCompProject.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeMCompProject_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_MCompProject;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_MCompProject()
+	{
+		if (!Z_Registration_Info_UPackage__Script_MCompProject.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/MCompProject",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0x2799BB91,
+				0x997EA70C,
+				METADATA_PARAMS(nullptr, 0)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_MCompProject.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_MCompProject.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_MCompProject(Z_Construct_UPackage__Script_MCompProject, TEXT("/Script/MCompProject"), Z_Registration_Info_UPackage__Script_MCompProject, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x2799BB91, 0x997EA70C));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Source/MCompProject/MCompProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MCompProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MCOMPPROJECT_API AMCompProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
